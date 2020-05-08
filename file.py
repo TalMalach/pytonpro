6b5e02e0e27c9ce12242d27cc8db53b4bf9b6c43
Tal Malach <talmalach@gmail.com>
2020-05-08 11:19:06 +0300

    My mini project with python

---
+++ file.py
@@ -0,0 +1,11 @@
+# python program to check an absolute value of a number
+
+def absolute_value(num):
+	"""this function returns the absolute
+	value of the entered number"""
+	
+	if num >= 0:
+		return num
+	
+	else:
+		return -num
